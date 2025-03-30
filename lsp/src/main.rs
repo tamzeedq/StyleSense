4851b681342a5d7c9d9cc94e3695ecfad0c86440
Diego <diegoarmstrong84@gmail.com>
2025-03-29 20:20:16 -0700

    Nit

--- lsp/src/main.rs
+++ lsp/src/main.rs
@@ -1,14 +1,3 @@
-mod parser;
-
 fn main() {
-    let code = r#"
-    #include <stdio.h>
-
-    int main() {
-        printf("Hello World"!)
-        return 0;
-    }
-    "#;
-
-    parser::parse_code(code, "c");
+    println!("Hello, LSP!");
 }
